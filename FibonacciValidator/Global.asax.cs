d216e095ef23036bab81c677bb0661d0c75d39f1
Nathan Dill <littlenate7@gmail.com>
2018-11-02 14:03:07 -0500

    Modified configuration to solve obselete error (fixed in earlier codebehind)

--- FibonacciValidator/Global.asax.cs
+++ FibonacciValidator/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Web.UI;
 
 namespace FibonacciValidator
 {
@@ -14,8 +15,19 @@ namespace FibonacciValidator
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-           AreaRegistration.RegisterAllAreas();
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            string JQueryVer = "1.7.1";
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-" + JQueryVer + ".min.js",
+                DebugPath = "~/Scripts/jquery-" + JQueryVer + ".js",
+                CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".min.js",
+                CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".js",
+                CdnSupportsSecureConnection = true,
+                LoadSuccessExpression = "window.jQuery"
+            });
         }
     }
 }
